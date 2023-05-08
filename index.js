718ef026f5916760d1592b40cdd4236e45901c4d
Jensen Lawless <jensenlawless@gmail.com>
2023-05-08 11:17:36 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const list = drivers.filter(function (n) {
+        return n.toLowerCase() === name.toLowerCase();
+    });
+    return list;
+};
+
+function fuzzyMatch(drivers, name) {
+    const list = drivers.filter(function (n) {
+        return n.startsWith(name)
+    });
+    return list
+}
+
+function matchName(drivers, name) {
+    const list = drivers.filter(function (n) {
+        return n.name === name
+    }
+    )
+    return list
+}
+
+
+
+
+
+
+
+
